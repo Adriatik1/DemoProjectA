774e9fbf2b78a8498a3c7d38bd73240db8155870
adriatik.ademi <adriatik.ademi@comitas.com>
2023-04-08 11:32:32 +0200

    role creation init

---
+++ DemoProjectA/Controllers/RolesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoProjectA.Controllers
+{
+    public class RolesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateNewRole()
+        {
+            return View();
+        }
+    }
+}
---
+++ Domain/Models/Abstractions/IdModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Abstractions
+{
+    public class IdModel
+    {
+        public Guid Id { get; set; }
+    }
+}
